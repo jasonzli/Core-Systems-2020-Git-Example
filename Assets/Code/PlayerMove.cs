14bd4b19b7b8628cd37bbc30c4eb916061b80181
artfail <artfailinc@gmail.com>
2020-12-09 14:57:17 -0500

    added simple 2D scene

---
+++ Assets/Code/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMove : MonoBehaviour
+{
+    public int speed = 4;
+    public int jumpForce = 800;
+
+    private Rigidbody2D _rigidbody;
+    public LayerMask groundLayer;
+    public Transform feetPoint;
+    public bool grounded;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float xSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
+    }
+
+    void Update()
+    {
+        if (isPaused)
+        {
+            return;
+        }
+        grounded = Physics2D.OverlapCircle(feetPoint.position, .5f, groundLayer);
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+            _rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    public void PausePlayer(bool pause)
+    {
+        isPaused = pause;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
