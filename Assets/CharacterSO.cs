73071d59cb33ce598fe6214f9fd4a90f809159ca
Jason Li <jasonli1337@gmail.com>
2020-12-09 20:09:00 -0500

    Scriptable Object Commit

---
+++ Assets/CharacterSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CharacterSO", menuName = "Core-Systems-2020-Git-Example/CharacterSO", order = 0)]
+public class CharacterSO : ScriptableObject {
+    public string Name;
+    public Texture2D avatar;
+    public string Title;
+}
